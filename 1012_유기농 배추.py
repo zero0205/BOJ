48b3ee69622add0c632b3a2f6b0c973f99b95909
zero0205 <etety39@naver.com>
2022-01-15 18:14:14 +0900

    1012_유기농 배추(인덱스에러)

---
+++ 1012_유기농 배추.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+# 상하좌우
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+def bfs(x, y, map_data):
+    que = deque([(x, y)])
+        
+    while que:
+        a, b = que.popleft()
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+
+            # 범위를 벗어나는지?
+            if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                continue
+
+            # 배추가 심어져있는지?
+            if map_data[nx][ny] == 1:
+                que.append((nx, ny))
+                map_data[nx][ny] = 0    # 이미 지나간 칸 표시
+
+
+t = int(input())    # 테스트케이스 개수
+
+for j in range(t):
+    m, n, k = map(int, input().split()) # 가로, 세로, 배추 위치 개수
+    map_data = [[0] * m for _ in range(n)]
+
+    # 배추 심어진 위치 입력받기
+    for i in range(k):
+        x, y = map(int, input().split())
+        map_data[y][x] = 1
+
+    cnt = 0
+
+    for col in range(m):
+        for row in range(n):
+            if map_data[row][col] == 1:
+                bfs(row, col, map_data)
+                cnt += 1
+
+    print(cnt)
