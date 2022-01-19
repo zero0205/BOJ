29c519ce3659de91103092d2faee560652cb4c4c
zero0205 <etety39@naver.com>
2022-01-19 09:37:41 +0900

    1920_수 찾기

---
+++ 1920_수 찾기.py
@@ -0,0 +1,24 @@
+def binary_search(array, target, start, end):
+    if start > end:
+        return False
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return True
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return binary_search(array, target, mid + 1, end)
+        
+
+n = int(input())
+arr1 = list(map(int, input().split()))
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+arr1.sort()
+
+for i in arr2:
+    if binary_search(arr1, i, 0, n - 1):
+        print("1")
+    else:
+        print("0")
